e54a4a282587294692b4792c61428fb66c9818e3
Rukieya Ainoo <a012702h@student.staffs.ac.uk>
2024-06-21 17:40:39 +0100

    So what I did was to check that the name is either Aged Brie or Backstage passes and then add 1 to quality if it is backstage passes and the backstage passes quality is equal to 49 (so that the highest quality it will add up to is 50) else if it is not equal to 49 which is if it is less than 49 then check if the sell by date is 10 or less if so add 2 to quality else if sell by date is 5 or less. then add 3 to quality else set quality to zero - Updated code version

--- gilded_rose.py
+++ gilded_rose.py
@@ -2,9 +2,9 @@
 
 class GildedRose(object):
 
-    def __init__(self, items, day):
+    def __init__(self, items):
         self.items = items
-        self.day = day
+        
     def update_quality(self):
         for item in self.items:
             if item.quality > 0 and item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
@@ -31,9 +31,7 @@ class GildedRose(object):
                         item.quality = item.quality + 3
                  if item.sell_in < 0 and item.name == "Backstage passes to a TAFKAL80ETC concert":
                         item.quality = 0
-               
             
-        
             item.sell_in = item.sell_in - 1
 class Item:
     def __init__(self, name, sell_in, quality):
