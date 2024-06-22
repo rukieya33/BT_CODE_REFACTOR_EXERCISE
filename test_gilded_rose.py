96fb3b8b43a6ed34d5ee270ab2028738ac4f7ee8
Rukieya Ainoo <a012702h@student.staffs.ac.uk>
2024-06-22 17:46:13 +0100

    Final code refactor

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -17,7 +17,62 @@ class GildedRoseTest(unittest.TestCase):
             gilded_rose.update_quality()
             self.assertEqual(19, items[0].quality)
 
+    def test_normal(self):
+                items = [
+                         Item("foo time", 25, 40),
+                         ]
+                gilded_rose = GildedRose(items)
+                gilded_rose.update_quality()
+                self.assertEqual(24, items[0].sell_in)
+                self.assertEqual(38, items[0].quality)
 
-        
+    def test_quality_never_negative(self):
+                items = [
+                         Item("foo time", 5, 0),
+                        ]
+                gilded_rose = GildedRose(items)
+                gilded_rose.update_quality()
+                self.assertEqual(4, items[0].sell_in)
+                self.assertEqual(0, items[0].quality)
+                
+    def test_max_quality(self):
+                items = [
+                         Item("foo time", 25, 49),
+                         Item("Aged Brie", 10, 50),
+                         Item("Backstage passes to a TAFKAL80ETC concert", 10, 50),
+                        ]
+                gilded_rose = GildedRose(items)
+                gilded_rose.update_quality()
+                self.assertEqual(24, items[0].sell_in)
+                self.assertEqual(50, items[0].quality)
+                self.assertEqual(9, items[1].sell_in)
+                self.assertEqual(50, items[1].quality)
+                self.assertEqual(9, items[2].sell_in)
+                self.assertEqual(50, items[2].quality)
+         
+    def test_aged_brie(self):
+                items = [
+                         Item("Aged Brie", 10, 40),
+                         ]
+                gilded_rose = GildedRose(items)
+                gilded_rose.update_quality()
+                self.assertEqual(9, items[0].sell_in)
+                self.assertEqual(41, items[0].quality)
+
+    def test_backstage_passes(self):
+                items = [
+                         Item("Backstage passes to a TAFKAL80ETC concert", 10, 45),
+                         Item("Backstage passes to a TAFKAL80ETC concert", 5, 15),
+                         Item("Backstage passes to a TAFKAL80ETC concert", -1, 30),
+                         ]
+                gilded_rose = GildedRose(items)
+                gilded_rose.update_quality()
+                self.assertEqual(9, items[0].sell_in)
+                self.assertEqual(47, items[0].quality)
+                self.assertEqual(4, items[1].sell_in)
+                self.assertEqual(18, items[1].quality)
+                self.assertEqual(-2, items[1].sell_in)
+                self.assertEqual(0, items[1].quality)
+
 if __name__ == '__main__':
     unittest.main()
